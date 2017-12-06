af9c63bf1ef78ef70f3ff7675e9684c1a64fd2d6
Stanislav <fox.alone1995@gmail.com>
2017-12-06 16:58:19 +0200

    Updated User model

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'surname', 'midname', 'datebirth', 'role_id', 'email', 'password',
     ];
 
     /**
