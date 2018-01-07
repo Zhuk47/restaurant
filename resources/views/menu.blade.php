de0265a0f9f46b98994d18a1c010c10705b974ca
Zhuk <zhukovskiy47@gmail.com>
2018-01-07 15:07:27 +0200

    menu corrections

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -12,20 +12,32 @@
     <div class="container">
         <center><h1>Меню</h1></center>
         @foreach($categories as $category)
-            <table>
-                <tr id="category"><div><h3>{{ $category->name }}</h3></div></tr>
-                @foreach($category->foods as $food)
-                    @foreach($food->foodPrice as $price)
-                        <div><b>{{ $food->name }}</b>
-                        /{{ $food->mass }}г./ {{ $price->price }}грн.
-                        @foreach($food->ingredients as $ingredient)
-                            <tr id="ingredients">{{ $ingredient->name }} /</tr>
-                        </div>
+            <center>
+                <table border="0">
+                    <tr id="category">
+                        <td colspan="3">
+                            <center><h4>{{ $category->name }}</h4></center>
+                        </td>
+                    </tr>
+                    @foreach($category->foods as $food)
+                        @foreach($food->foodPrice as $price)
+                            <tr>
+                                <td width="500"><b>{{ $food->name }}</b></td>
+                                <td width="100">/{{ $food->mass }} г./</td>
+                                <td width="100">{{ $price->price }} грн.</td>
+                            </tr>
+                            <tr id="ingredients">
+                                <td colspan="3">(
+                                    @foreach($food->ingredients as $ingredient)
+                                        {{$ingredient->name}} /
+                                    @endforeach
+                                )</td>
+                            </tr>
                         @endforeach
                     @endforeach
-                @endforeach
-                @endforeach
-            </table>
+                    @endforeach
+                </table>
+            </center>
     </div>
 
 @endsection
