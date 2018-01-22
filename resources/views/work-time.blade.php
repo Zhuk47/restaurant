79cd6416d4b5aff7abab7e03254ccc0099ac2be9
stanislav <farquad111@outlook.com>
2018-01-22 16:37:58 +0200

    updated work-time view

--- resources/views/work-time.blade.php
+++ resources/views/work-time.blade.php
@@ -1,40 +1,26 @@
-@extends('adminViews.home')
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.16/addons/p5.dom.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.16/p5.min.js"></script>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
       integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-      integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+@extends('adminViews/home')
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-        crossorigin="anonymous"></script>
 @section('content')
     <div class="container">
-        <h1>Сотрудники на работе</h1>
-
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>ФИО</th>
-                <th>Должность</th>
-                <th>Дата рождения</th>
-                <th>Адрес электронной почты</th>
-            </tr>
-            </thead>
-            <tbody>
-            @if($users)
-                @foreach($users as $user)
-                    <tr>
-                        @if($user->isOnline())
+        <form class="form-order" method="get" action="{{url ('/')}}">
+            <h2 class="form-order-heading">Сотрудники онлайн</h2>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>ФИО</th>
+                    <th>Должность</th>
+                    <th>Статус</th>
+                    <th>Провел на работе с момента входа в систему</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach(\App\Visit::all() as $user)
+                    @if($user->goneaway === null)
+                        <tr>
                             <td>{{ $user->surname }} {{$user->name}} {{$user->midname}}</td>
                             @if($user->role_id == 1)
                                 <td>Администратор</td>
@@ -43,16 +29,28 @@
                             @elseif($user->role_id == 3)
                                 <td>Повар</td>
                             @endif
-                            <td>{{$user->dateBirth}}</td>
-                            <td>{{$user->email}}</td>
-                        @endif
-                        @endforeach
-                    </tr>
-            </tbody>
-        </table>
-        @endif
-
+                            <td>Онлайн</td>
+                            <td><?php
+                                if ($user->goneaway == NULL) {
+                                    $now = \Illuminate\Support\Carbon::now();
+                                    $time = \Illuminate\Support\Carbon::parse($user['entered']);
+                                    $res = $now->diffInMinutes($time);
+                                    echo $res;
+                                    }
+                                ?></td>
+                            @endif
+                            @endforeach
+                            <br><br>
+                </tbody>
+            </table>
+            <button onclick="goBack()" class="btn btn-lg btn-primary btn-block">
+                Назад
+            </button>
+            <script>
+                function goBack() {
+                    window.history.back();
+                }
+            </script>
+        </form>
     </div>
-
-
 @endsection
